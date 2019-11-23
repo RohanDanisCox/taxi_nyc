f4bfcff4f17ea6428828030d4bf16fa992f829fa
rohan_gcp <rohan.daniscox@gmail.com>
2019-11-23 04:44:45 +0000

    accessed data from Google Drive

---
+++ access_data.r
@@ -0,0 +1,18 @@
+# Initialise and obtain data from google drive
+
+library(googledrive)
+library(tidyverse)
+
+# Connect to my google drive where the files have been shared
+drive_auth(use_oob = TRUE)
+
+# Identify the filenames
+drive_find()
+
+# Download the zip files
+drive_download("trip_data_4.csv.zip")
+drive_download("trip_fare_4.csv.zip")
+
+# Unzip the files
+unzip("trip_data_4.csv.zip")
+unzip("trip_fare_4.csv.zip")
