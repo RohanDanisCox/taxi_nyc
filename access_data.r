cd68e57d63d0a76c0d816faa7bc529cf859e31ab
rohan_gcp <rohan.daniscox@gmail.com>
2019-11-24 01:31:46 +0000

    Forgot to update access data

--- access_data.r
+++ access_data.r
@@ -16,3 +16,7 @@ drive_download("trip_fare_4.csv.zip")
 # Unzip the files
 unzip("trip_data_4.csv.zip")
 unzip("trip_fare_4.csv.zip")
+
+# Download the taxi_zone data
+taxi_zones_names <- drive_find("taxi_zones.")
+map(taxi_zones_names$name,drive_download)
