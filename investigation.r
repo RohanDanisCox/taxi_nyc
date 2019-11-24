c911fe21c9068078f68613fbff07660784d3d479
rohan_gcp <rohan.daniscox@gmail.com>
2019-11-24 01:29:54 +0000

    Answered a few more questions

--- investigation.r
+++ investigation.r
@@ -7,8 +7,8 @@
   library(lubridate)
   library(feather)
   library(sf)
+  library(scales)
   
-
 # ---- 2) Load data ----
 
   trip_data_raw <- read_csv("trip_data_4.csv")
@@ -82,10 +82,13 @@
 
   summary(train$passenger_count)
   
+  train %>% summarise(mean = mean(passenger_count)) %>% pull() %>% round(2)
+  
   ggplot(train, aes(passenger_count)) +
     geom_bar() + 
     scale_x_continuous(breaks = c(1:6)) + 
     ggtitle("Distribution of Passengers") +
+    labs(x = "Number of Passengers") +
     theme_minimal()
     
   # ---- 6) b. What is the distribution of payment_type?   ---- 
@@ -93,9 +96,52 @@
   ggplot(train, aes(payment_type)) +
     geom_bar() + 
     ggtitle("Distribution of Payment Type") +
+    labs(x = "Payment Method", y = "Frequency") +
     theme_minimal()
+  
+  # how does it relate to tip amount
+  
+  ggplot(train,aes(tip_amount, fill = payment_type)) +
+    geom_histogram(bins = 500) + 
+    coord_cartesian(xlim = c(0,20),ylim=c(0, 800000))+
+    scale_x_continuous(breaks = c(1:20)) +
+    scale_y_continuous(labels = comma) 
+
 
   # ---- 6) c. What is the distribution of fare amount?   ---- 
+  summary(train$fare_amount)
+  
+  train %>% summarise(median(fare_amount)) %>% pull() %>% round(2)
+  train %>% summarise(mean(fare_amount)) %>% pull() %>% round(2)
+  
+  ggplot(train, aes(fare_amount)) +
+    geom_histogram(bins = 500) + 
+    #coord_cartesian(xlim = c(0,100),ylim=c(0, 800000))+
+    #scale_x_continuous(breaks = c(1:20)) +
+    scale_y_continuous(labels = comma) +
+    ggtitle("Distribution of Fare Amount") +
+    labs(x = "Fare Amount", y = "Frequency") +
+    theme_minimal()
+  
+  train_2 %>% select(fare_amount,pickup_zone) %>% filter(fare_amount == 52) %>% 
+    count(pickup_zone) %>% arrange(desc(n)) %>% head(1) %>% select(n) %>% pull()
+  train_2 %>% select(fare_amount,dropoff_zone) %>% filter(fare_amount == 52) %>% 
+    count(dropoff_zone) %>% arrange(desc(n)) %>% head(1) %>% select(n) %>% pull()
+  
+  pickup_JFK <- train_2 %>%
+    select(fare_amount,pickup_zone,dropoff_borough) %>%
+    filter(fare_amount == 52) %>%
+    filter(pickup_zone == "JFK Airport") %>%
+    count(dropoff_borough) %>%
+    arrange(desc(n))
+    
+  dropoff_JFK <- train_2 %>%
+    select(fare_amount,dropoff_zone,pickup_borough) %>%
+    filter(fare_amount == 52) %>%
+    filter(dropoff_zone == "JFK Airport") %>%
+    count(pickup_borough) %>%
+    arrange(desc(n))
+
   
   ggplot(train, aes(fare_amount)) +
     geom_density() + 
@@ -105,22 +151,48 @@
   summary(train$fare_amount)
   
   # ---- 7) d. What is the distribution of tip amount?   ---- 
+  summary(train$tip_amount)
   
-  ggplot(train, aes(tip_amount)) +
-    geom_density() + 
-    ggtitle("Distribution of Tip Amount") +
+  ggplot(train,aes(tip_amount)) +
+    geom_histogram(bins = 500) + 
+    ggtitle("Distribution of tip amount") +
+    labs(x = "Tip Amount", y = "Frequency") +
+    theme_minimal()
+    
+  ggplot(train,aes(tip_amount, fill = payment_type)) +
+    geom_histogram(bins = 500) + 
+    coord_cartesian(xlim = c(0,20),ylim=c(0, 800000))+
+    scale_x_continuous(breaks = c(1:20)) +
+    scale_y_continuous(labels = comma) +
+    ggtitle("Only Card Tips are Recorded") +
+    labs(x = "Tip Amount", y = "Frequency") +
     theme_minimal()
   
-  summary(train$tip_amount)
+  card_tip <- train %>%
+    select(tip_amount,payment_type) %>%
+    filter(payment_type == "CRD")
+  
+  train %>% select(tip_amount,payment_type) %>%
+    filter(payment_type == "CRD")%>% summarise(mean = mean(tip_amount)) %>% pull() %>% round(2)
+  
+  ggplot(card_tip, aes(tip_amount)) +
+    geom_histogram(bins = 500) + 
+    coord_cartesian(xlim = c(0,20)) +
+    ggtitle("Distribution of Tip Amount When Paid With Card") +
+    labs(x = "Tip Amount") +
+    theme_minimal()
   
   # ---- 7) e. What is the distribution of total amount?   ---- 
   
   summary(train$total_amount)
   
   ggplot(train, aes(total_amount)) +
-    geom_density() + 
+    geom_histogram(bins = 500) + 
+    scale_y_continuous(labels = comma) +
     ggtitle("Distribution of Total Amount") +
+    labs(x = "Total Amount", y = "Frequency") +
     theme_minimal()
+
   
   # ---- 8) f. What are top 5 busiest hours of the day? ---- 
 
@@ -134,12 +206,29 @@
   ggplot(busy_hours,aes(x = pickup_hour,y= count, fill = top_5)) +
     geom_bar(stat = "identity") +
     scale_fill_manual(values=c("grey","red")) +
+    scale_y_continuous(labels = comma) +
     ggtitle("Top 5 Busiest Hours - 6-11pm") +
+    labs(fill = "Top 5", x = "Pickup Hour", y = "Frequency") + 
     theme_minimal()
   
-  # ---- 8) g. What are the top 10 busiest locations of the city?  ---- 
+# ---- 8) g. What are the top 10 busiest locations of the city?  ---- 
 
- # Add in taxi zones
+  # What it looks like to begin with 
+    
+    distinct_pickup <- train %>% distinct(pickup_longitude, pickup_latitude) %>% base::sample(5)
+    sample_index <- sample(1:nrow(distinct_pickup), nrow(distinct_pickup)/100)
+    distinct_sample <- distinct_pickup[sample_index,]
+
+    ggplot(distinct_sample, aes(x = pickup_longitude, y = pickup_latitude)) + 
+      geom_point(alpha = 0.3, size = 0.01) + 
+      xlim(-74.05,-73.85) + 
+      ylim(40.65,40.85) + 
+      ggtitle("Coordinates of Pickups") +
+      labs(x = "Longitude", y = "Latitude") + 
+      theme_minimal()
+    
+# ---- 8a) Add in Taxi Zones  ---- 
+    
   taxi_zones <- read_sf("taxi_zones") %>%
     st_transform(4326)
   
@@ -181,10 +270,12 @@
   
   train_2 <- post_dropoff
   
-  write_feather(train_2,"train_2.feather")
+  write_feather(train_2,"split_data/train_2.feather")
   
   names(train_2)
 
+  # Show the busiest locations
+  
   busy_pickup_locations <- train_2 %>%
     group_by(pickup_zone) %>%
     summarise(pickup= n()) 
@@ -201,11 +292,21 @@
     mutate(top_10 = case_when(rank <= 10 ~ "Yes",
                              TRUE ~ "No"))
   
+  busy_overall %>% filter(top_10 == "Yes") %>% arrange (desc(overall)) %>% select(zone, trips = overall)
+  
   taxi_zone_busy <- taxi_zones %>%
     left_join(busy_overall, by = "zone")
   
+  saveRDS(taxi_zone_busy,"taxi_zone_busy.rds")
+  
+  taxi_zone_borough <- taxi_zones %>%
+    group_by(borough) %>%
+    summarise(n = n())
+  
   ggplot(taxi_zone_busy) + 
-    geom_sf(mapping = aes(fill = overall)) +
-    scale_fill_gradient(low = "white", high = "red") + 
-    theme_minimal()
+    geom_sf(mapping = aes(fill = overall, colour = borough)) +
+    scale_fill_gradient(low = "white", high = "red", label = comma) + 
+    ggtitle("Taxi Zone Pickups") +
+    labs(x = "Longitude", y = "Latitude", fill = "Number of Pickups", colour = "Borough") + 
+    theme_minimal() 
  
